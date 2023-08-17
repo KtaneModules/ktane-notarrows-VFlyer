80d5a2d82e2f764aa021c9eefd98b80494eebb2b
Vincent Flyer (Vincent Chan) <35939457+VFlyer@users.noreply.github.com>
2023-08-17 11:14:11 -0700

    Alright. Here We Go...

--- Assets/Scripts/Grid2DFiller.cs
+++ Assets/Scripts/Grid2DFiller.cs
@@ -5,21 +5,23 @@ using UnityEngine;
 
 public class Grid2DFiller : MonoBehaviour {
 
-	public int[] grid;
+	public string gridA, gridB;
+	public bool stitchGrids;
 	public int squareLength = 10;
 	// Use this for initialization
 	void Start () {
-		retryGen:
-		var endingGrid = Enumerable.Repeat(-1, squareLength * squareLength).ToArray();
-		var remainingPossibilities = new List<int>[squareLength * (squareLength - 1)];
-		for (var x = 0; x < squareLength * squareLength - 1; x++)
-			remainingPossibilities[x] = Enumerable.Range(0, squareLength).ToList();
-		var firstRowSets = Enumerable.Range(0, squareLength).ToArray().Shuffle();
-		for (var x = 0; x < squareLength; x++)
-			endingGrid[x] = firstRowSets[x];
-		for (var x = 0; x < remainingPossibilities.Length; x++)
-        {
-
-        }
+		if (!stitchGrids)
+		{
+			var resulingGrid = Enumerable.Range(0, squareLength * squareLength).ToArray();
+			do
+				resulingGrid.Shuffle();
+			while (Enumerable.Range(0, squareLength).Any(a =>
+			 resulingGrid.Skip(squareLength * a).Take(squareLength).Select(b => b % squareLength).Distinct().Count() < squareLength ||
+			 resulingGrid.Skip(squareLength * a).Take(squareLength).Select(b => b / squareLength).Distinct().Count() < squareLength ||
+			 Enumerable.Range(0, squareLength).Select(b => resulingGrid[squareLength * b + a]).Select(b => b / squareLength).Distinct().Count() < squareLength ||
+			 Enumerable.Range(0, squareLength).Select(b => resulingGrid[squareLength * b + a]).Select(b => b % squareLength).Distinct().Count() < squareLength
+			));
+			Debug.Log(resulingGrid.Join(","));
+		}
 	}
 }
